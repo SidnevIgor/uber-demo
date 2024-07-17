9e7c5bbbe5e14cc0a3a3cfa05acb429ac0b0367d
i.sidnev <i.sidnev@smartex-it.com>
2024-07-17 18:35:41 +0300

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Igor Sidnev on 17/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
